5c5af30df1f58769d316a6a6c1e30cd5a3963b0b
罗念笙 <1079936145@qq.com>
2022-07-18 10:28:15 +0800

    init

---
+++ src/test/java/com/al_tair/storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al_tair.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
